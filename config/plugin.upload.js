42f0be55c8759b4fa05d89b1591ef9678ab0d432
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2022-01-24 15:31:10 +0100

    ajout du plugin upload pour télécharger les fichiers

---
+++ config/plugin.upload.js
@@ -0,0 +1,10 @@
+const {baseUrl} = require('../config/env')
+
+module.exports = {
+	/**
+	 * URL de base pour la génération des urls des fichiers uploadés
+	 * 
+	 * @var string
+	 */
+	baseUrl: baseUrl
+}
